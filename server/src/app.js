d0ecf4287404ef658a789b5508257ce93a150826
Pankajshahare <pankajsahare.04@gmail.com>
2026-03-28 23:15:58 +0530

    Fix cors error-3

--- server/src/app.js
+++ server/src/app.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(helmet());
 app.use(
   cors({
-    origin: env.FRONTEND_URL,
+    origin: "*",
     credentials: true,
     methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
